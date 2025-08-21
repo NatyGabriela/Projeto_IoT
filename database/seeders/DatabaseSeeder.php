212c3008f8fad08bc3fceabe2a050d405c6e91c8
Natygabriela <nataliaggb007@gmail.com>
2025-08-21 14:06:35 -0300

    Dashboard-style-app-blade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Ambiente;
+use App\Models\Registro;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AmbienteSeeder::class,
-            SensorSeeder::class
+            SensorSeeder::class,
+            RegistroSeeder::class
         ]);
     }
 }
--- database/seeders/RegistroSeeder.php
+++ database/seeders/RegistroSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Registro;
 use App\Models\Sensor;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -28,7 +29,7 @@ class RegistroSeeder extends Seeder
         $dataAtual = Carbon::now('America/Sao_Paulo')->subMonth();
         $dataFinal = Carbon::now('America/Sao_Paulo');
 
-        while($dataAtual->lessThaOrEqualto($dataFinal)){
+        while($dataAtual->lessThanOrEqualto($dataFinal)){
         foreach($sensores as $sensor){
          $tipo = $sensor->tipo;
         $unidade =$unidadesPorTipo[$tipo] ?? '';
@@ -39,8 +40,25 @@ class RegistroSeeder extends Seeder
         case 'umidade':
          $valor= $faker->randomFloat(2,20,90);
         break;
+        case 'luminosidade': 
+            $valor = $faker->numberBetween(0, 1000);
+            break;
+            case 'presenca':
+                $valor= $faker->randomElement(['ON', 'OFF']);
+                break;
+                default;
+                $valor=$faker->randomFloat(2, 0, 100);
+                break;
                 }
+
+                Registro::create([
+                    'sensor_id'=> $sensor->id,
+                    'valor'=> $valor,
+                    'unidade'=>$unidade,
+                    'data_hora'=> $dataAtual->format('Y-m-d H:i:s')
+                ]);
             }
+            $dataAtual->addMinutes(10);
         }
 
     }
